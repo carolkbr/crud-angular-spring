295a1993ffce08dbb24aceb13c9ab8699926828d
Caroline Rolim <ckbrolim@gmail.com>
2021-10-28 17:35:05 -0300

    Adiciona chamada HTTP para obter lista de cursos

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,6 +1,7 @@
 import { CoursesService } from './../services/courses.service';
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../model/course';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-courses',
@@ -9,7 +10,7 @@ import { Course } from '../model/course';
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Course[] = [];
+  courses: Observable<Course[]>;
   displayedColumns = ['name', 'category'];
 
   constructor(private coursesService: CoursesService) {
--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { first, tap } from 'rxjs/operators'
 import { Course } from './../model/course';
 
 @Injectable({
@@ -8,11 +8,13 @@ import { Course } from './../model/course';
 })
 export class CoursesService {
 
+  private readonly API = '/assets/courses.json';
   constructor(private httpClient: HttpClient) { }
 
-  list(): Course[] {
-    return [
-      {_id: '1', name: 'Angular', category: 'Front-end'}
-    ]; 
+  list() {
+    return this.httpClient.get<Course[]>(this.API).pipe(
+      first(),
+      tap(courses => console.log(courses))
+    );
   }
 }
