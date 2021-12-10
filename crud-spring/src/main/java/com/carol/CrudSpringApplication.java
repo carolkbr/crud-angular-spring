21080de5c4751dbba5dfe99c491a91b59167d443
Caroline Rolim <ckbrolim@gmail.com>
2021-12-10 18:57:58 -0300

    Cria método GET da API

---
+++ crud-spring/src/main/java/com/carol/CrudSpringApplication.java
@@ -0,0 +1,29 @@
+package com.carol;
+
+import com.carol.model.Course;
+import com.carol.repository.CourseRepository;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CrudSpringApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner initDatabase(CourseRepository courseRepository) {
+		return args -> {
+			courseRepository.deleteAll();
+
+			Course c = new Course();
+			c.setName("Angular com Spring");
+			c.setCategory("front-end");
+
+			courseRepository.save(c);
+		};
+	}
+}
---
+++ crud-spring/src/main/java/com/carol/controller/CourseController.java
@@ -0,0 +1,25 @@
+package com.carol.controller;
+
+import java.util.List;
+
+import com.carol.model.Course;
+import com.carol.repository.CourseRepository;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/api/courses")
+@AllArgsConstructor
+public class CourseController {
+    
+    private final CourseRepository courseRepository;
+
+    @GetMapping
+    public List<Course> list(){
+        return courseRepository.findAll();
+    }
+}
